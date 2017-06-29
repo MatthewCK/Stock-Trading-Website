6046c95ea9eccedf88813645dd08408df35cad3d
MatthewCK <MatthewCK@users.noreply.github.com>
2017-06-29 03:16:09 +0000

    20170629T031517Z

---
+++ initials.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string s = get_string();
+    
+    printf("%c", toupper(s[0]));
+    
+    
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        
+        if (s[i] == ' ')
+        {
+            printf("%c", toupper(s[i+1]));
+        }
+    }
+    
+    printf("\n");
+    
+  //  for (int i = 0; )
+}
